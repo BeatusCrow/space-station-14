f4c7c859ac69390b6ebe0d9510c1ecae52a7717e
BeatusCrow <ivan.poluhtin.06@bk.ru>
2024-07-07 16:07:37 +0300

    07.07.24
    deleting useless text

--- Content.Server/TeleportationZone/LandingPointComponent.cs
+++ Content.Server/TeleportationZone/LandingPointComponent.cs
@@ -7,11 +7,5 @@ namespace Content.Server.TeleportationZone
     [RegisterComponent]
     public sealed partial class LandingPointComponent : Component
     {
-
-        [DataField("name")]
-        public string NameLandingPoint = "unknow"; // The name of the point that the player will see when activating the TeleportationZone console
-
-        [DataField("uin")]
-        public int UIN = 1; // The unique identification number of the point (each must have its own unique one!)
     }
 }
